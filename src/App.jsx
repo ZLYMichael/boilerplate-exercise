12c63721a351559ea2b19e499a1544870c0070cf
Michael Zhu <zlymike@outlook.com>
2017-07-03 18:26:36 +0000

    removed unused code

--- src/App.jsx
+++ src/App.jsx
@@ -29,18 +29,6 @@ class App extends Component {
       userCount: 0,
       };
    
-    // componentDidMount() {
-    //   console.log("componentDidMount <App />");
-    //   setTimeout(() => {
-    //     console.log("Simulating incoming message");
-    //     // Add a new message to the list of messages in the data store
-    //     const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-    //     const messages = this.state.messages.concat(newMessage)
-    //     // Update the state of the app component.
-    //     // Calling setState will trigger a call to render() in App and all child components.
-    //     this.setState({messages: messages})
-    //   }, 3000);
-    // }
 
   componentDidMount() {
     this.setState({
